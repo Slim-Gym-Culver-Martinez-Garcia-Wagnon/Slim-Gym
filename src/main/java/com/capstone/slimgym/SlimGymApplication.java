a23b8143562673c13cedf171644a3b28709b25d2
David Wagnon <david.wagnon02@gmail.com>
2021-08-06 13:34:10 -0500

    Second push:

---
+++ src/main/java/com/capstone/slimgym/SlimGymApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.slimgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlimGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlimGymApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/slimgym/SlimGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.slimgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlimGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
