ae6eea6426dba64db5c266e0407d16c791fb3b51
David Culver <culver.david.p@gmail.com>
2021-08-09 17:20:48 -0500

    began user edit

--- src/main/java/com/capstone/slimgym/controllers/UserController.java
+++ src/main/java/com/capstone/slimgym/controllers/UserController.java
@@ -1,12 +1,15 @@
 package com.capstone.slimgym.controllers;
 
+import com.capstone.slimgym.models.Gym;
 import com.capstone.slimgym.models.User;
 import com.capstone.slimgym.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -32,4 +35,30 @@ public class UserController {
         users.save(user);
         return "redirect:/login";
     }
+
+
+    @GetMapping("/user/{id}/edit")
+    public String userToEdit(@PathVariable long id, Model model) {
+//        checks to see if the user is logged in and has authentication
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User updatedUser =  users.getById(id);
+        if(user == updatedUser){
+            model.addAttribute("user", users.getById(id));
+            model.addAttribute("id", id);
+            return "editUser";
+        }
+        else {
+            return "login";
+        }
+    }
+
+
+    @PostMapping("/user/edit/update/{id}")
+    public String editUser(@PathVariable long id, @ModelAttribute User user) {
+        User updateUser = users.findById(user.getId());
+//        user.setUser(updateUser);
+        users.save(updateUser);
+        return "login";
+    }
+
 }
