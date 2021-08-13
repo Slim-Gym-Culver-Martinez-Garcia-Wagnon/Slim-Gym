6e3c11df66114b13479bf2ec2af38ad7883471a0
Juan Garcia <garcia.d.juan14@gmail.com>
2021-08-13 16:41:41 -0500

    created review post

--- src/main/java/com/capstone/slimgym/SecurityConfiguration.java
+++ src/main/java/com/capstone/slimgym/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                     .authorizeRequests()
                     .antMatchers(
-                            "/posts/create", "/profile","/users/{id}/edit")
+                            "/posts/create", "/profile","/users/{id}/edit", "/create-review")
                     .authenticated()
 
                 .and()
--- src/main/java/com/capstone/slimgym/controllers/UserController.java
+++ src/main/java/com/capstone/slimgym/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.capstone.slimgym.controllers;
 
 import com.capstone.slimgym.models.User;
+import com.capstone.slimgym.repositories.PostRepository;
 import com.capstone.slimgym.repositories.ReviewRepository;
+import com.capstone.slimgym.repositories.ScheduleRepository;
 import com.capstone.slimgym.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,13 +18,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 
     private UserRepository users;
+    private PostRepository postDao;
     private ReviewRepository reviewDao;
     private PasswordEncoder passwordEncoder;
+    private ScheduleRepository scheduleDao;
 
-    public UserController(UserRepository users, PasswordEncoder passwordEncoder, ReviewRepository reviewDao) {
+    public UserController(UserRepository users, PasswordEncoder passwordEncoder, ReviewRepository reviewDao, PostRepository postDao, ScheduleRepository scheduleDao) {
         this.users = users;
         this.passwordEncoder = passwordEncoder;
         this.reviewDao = reviewDao;
+        this.postDao = postDao;
+        this.scheduleDao = scheduleDao;
     }
 
     @GetMapping("/sign-up")
@@ -67,25 +73,10 @@ public class UserController {
     @GetMapping("/profile")
     public String userProfile(Model model){
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        long userId = user.getId();
-        model.addAttribute("reviews", reviewDao.findByUserId(userId));
+        model.addAttribute("reviews", reviewDao.findByUser(user));
+        model.addAttribute("gyms", postDao.findByUser(user));
+        model.addAttribute("events", scheduleDao.findAllByGymUser(user));
         return "profile";
     }
-
-
-
-
-//        @GetMapping("/user/{id}/profile")
-//    public String userProfile(@PathVariable long id, Model model) {
-//        User user = users.getById(id);
-//        boolean theUser = false;
-//            if(SecurityContextHolder.getContext().getAuthentication().getPrincipal() != "anonymousUser") {
-//                User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//                theUser = currentUser.getId() == user.getUsername().findById();
-//            }
-//            model.addAttribute("user", user);
-//            model.addAttribute("theUser", theUser);
-//            return "profile";
-//    }
 
 }
