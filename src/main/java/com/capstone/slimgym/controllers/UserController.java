24c63e7430e9229ff376f1a80d86052858453a5f
David Wagnon <david.wagnon02@gmail.com>
2021-08-30 02:34:44 -0500

    Styled gym-page

--- src/main/java/com/capstone/slimgym/controllers/UserController.java
+++ src/main/java/com/capstone/slimgym/controllers/UserController.java
@@ -99,6 +99,7 @@ public class UserController {
         User userOwner = users.findById(user.getId());
         List<Review> reviews = reviewDao.findByUserId(user.getId());
         List<Picture> gymPictures = pictureDao.findAll();
+
         if(SecurityContextHolder.getContext().getAuthentication().getPrincipal() == "anonymousUser"){
             return "redirect:/login";
         }
